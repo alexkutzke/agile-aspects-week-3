10687f52e9acf720ede79e931bd58cc71af5d2c9
Jorge Hernandesm <jorgehernandes88@gmail.com>
2021-08-14 17:44:20 -0300

    Exercise 14.08.2021 - Jorge Hernandes

--- exercise.js
+++ exercise.js
@@ -32,69 +32,36 @@ function main() {
     },
   ];
 
-  processTransactions(transactions);
+  CheckForTransactions(transactions);
 }
 
-function processTransactions(transactions) {
+function CheckForTransactions(transactions) {
   if (transactions && transactions.length > 0) {
-    for (const transaction of transactions) {
-      if (transaction.type === 'PAYMENT') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardPayment(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalPayment(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanPayment(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else if (transaction.type === 'REFUND') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardRefund(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalRefund(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanRefund(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else {
-        console.log('Invalid transaction type!', transaction);
-      }
-    }
+    transactionType(transactions);
   } else {
     console.log('No transactions provided!');
   }
 }
 
-function processCreditCardPayment(transaction) {
-  console.log(
-    'Processing credit card payment for amount: ' + transaction.amount
+function transactionType(transactions) {
+    for (const transaction of transactions) {
+        if (transaction.status === 'OPEN') {
+            returnTransaction(transaction)
+        } else {
+          console.log('Invalid transaction type!', transaction);
+        }
+      }
+}
+
+function returnTransaction(transaction) {
+   console.log(
+    'Processing '+ OrganizeStringForReturn(transaction.method)  + ' ' + transaction.type + ' for amount: ' + transaction.amount
   );
 }
 
-function processCreditCardRefund(transaction) {
-  console.log(
-    'Processing credit card refund for amount: ' + transaction.amount
-  );
-}
-
-function processPayPalPayment(transaction) {
-  console.log('Processing PayPal payment for amount: ' + transaction.amount);
-}
-
-function processPayPalRefund(transaction) {
-  console.log('Processing PayPal refund for amount: ' + transaction.amount);
-}
-
-function processPlanPayment(transaction) {
-  console.log('Processing plan payment for amount: ' + transaction.amount);
-}
-
-function processPlanRefund(transaction) {
-  console.log('Processing plan refund for amount: ' + transaction.amount);
+function OrganizeStringForReturn(text){
+    text = text.replace("_", " ");
+    return text.toLowerCase().split(' ').map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(' ')
 }
