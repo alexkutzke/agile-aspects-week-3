0bac2c9b1f78006c9e95e0eceea5711c091ac29e
João Vitor Balzer <joaobalzer@gmail.com>
2021-08-21 11:17:07 -0300

    Refatoração feita pelo aluno Joãp Vitor Balzer

--- exercise.js
+++ exercise.js
@@ -1,6 +1,6 @@
-main();
+getTransactions();
 
-function main() {
+function getTransactions() {
   const transactions = [
     {
       id: 't1',
@@ -31,70 +31,112 @@ function main() {
       amount: '15.99',
     },
   ];
-
   processTransactions(transactions);
 }
 
-function processTransactions(transactions) {
-  if (transactions && transactions.length > 0) {
-    for (const transaction of transactions) {
-      if (transaction.type === 'PAYMENT') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardPayment(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalPayment(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanPayment(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else if (transaction.type === 'REFUND') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardRefund(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalRefund(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanRefund(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else {
-        console.log('Invalid transaction type!', transaction);
-      }
-    }
-  } else {
-    console.log('No transactions provided!');
+function processTransactions(transactions) {   
+    testTransacitonsLenght(transactions)
+    transactions.forEach(processPaymentStatus);
+}
+
+function testTransacitonsLenght(transactions){
+  if (transactions.length <= 0 || !transactions) {
+  console.log('No transactions provided!');
   }
 }
 
+function processPaymentStatus(transaction){
+
+      if (transaction.status === 'OPEN') {
+          processPaymentType(transaction)
+      } 
+        
+      else {
+          invalidTransactionType(transaction)
+        }
+       
+}
+
+function processPaymentType(transaction){
+
+      if (transaction.type === 'PAYMENT') {
+          processPaymentMethod(transaction)
+      } 
+
+      else if (transaction.type === 'REFUND') {
+          processRefundMethod(transaction)
+      } 
+        
+      else {
+          invalidTransactionType(transaction)
+        }
+       
+}
+
+function processPaymentMethod(transaction){
+
+    if (transaction.method === 'CREDIT_CARD') {
+            processCreditCardPayment(transaction);
+          } 
+
+    else if (transaction.method === 'PAYPAL') {
+            processPayPalPayment(transaction);
+          } 
+
+    else if (transaction.method === 'PLAN') {
+            processPlanPayment(transaction);
+          }
+
+    else {
+          invalidTransactionType(transaction)
+        }
+       
+}
+
+function processRefundMethod(transaction){
+
+    if (transaction.method === 'CREDIT_CARD') {
+      processCreditCardRefund(transaction);
+    } 
+
+    else if (transaction.method === 'PAYPAL') {
+      processPayPalRefund(transaction);
+    } 
+
+    else if (transaction.method === 'PLAN') {
+      processPlanRefund(transaction);
+    }
+
+    else {
+          invalidTransactionType(transaction)
+        }
+       
+}
+
+function invalidTransactionType(transaction){
+  console.log('Invalid transaction type!', transaction)
+}
+
 function processCreditCardPayment(transaction) {
-  console.log(
-    'Processing credit card payment for amount: ' + transaction.amount
-  );
+  console.log(`Processing credit card payment for amount: US$ ${transaction.amount}`);
 }
 
 function processCreditCardRefund(transaction) {
-  console.log(
-    'Processing credit card refund for amount: ' + transaction.amount
-  );
+  console.log(`Processing credit card refund for amount: US$ ${transaction.amount}`);
 }
 
 function processPayPalPayment(transaction) {
-  console.log('Processing PayPal payment for amount: ' + transaction.amount);
+  console.log(`Processing PayPal payment for amount: US$ ${transaction.amount}`);
 }
 
 function processPayPalRefund(transaction) {
-  console.log('Processing PayPal refund for amount: ' + transaction.amount);
+  console.log(`Processing PayPal refund for amount: US$ ${transaction.amount}`);
 }
 
 function processPlanPayment(transaction) {
-  console.log('Processing plan payment for amount: ' + transaction.amount);
+  console.log(`Processing plan payment for amount: US$ ${transaction.amount}`);
 }
 
 function processPlanRefund(transaction) {
-  console.log('Processing plan refund for amount: ' + transaction.amount);
+  console.log(`Processing plan refund for amount: US$ ${transaction.amount}`);
 }
