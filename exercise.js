62fc7394a03baefc32f7c6bc888417eef518b8a2
tozamba <tozamba@hotmail.com>
2023-08-26 19:24:44 -0300

    Caue, Aruake exercise.js

--- exercise.js
+++ exercise.js
@@ -1,5 +1,6 @@
-main();
-
+// Cauê Veiga e Aruake Mourão
+//
+// Função principal que inicia o processamento das transações
 function main() {
   const transactions = [
     {
@@ -35,52 +36,83 @@ function main() {
   processTransactions(transactions);
 }
 
+// Função para processar todas as transações
 function processTransactions(transactions) {
-  if (transactions && transactions.length > 0) {
-    for (const transaction of transactions) {
-      if (transaction.type === 'PAYMENT') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardPayment(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalPayment(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanPayment(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else if (transaction.type === 'REFUND') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardRefund(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalRefund(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanRefund(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else {
-        console.log('Invalid transaction type!', transaction);
-      }
-    }
-  } else {
+  // Verifica se há transações para processar
+  if (transactions.length === 0) {
     console.log('No transactions provided!');
+    return;
+  }
+
+  // Processa cada transação individualmente
+  for (const transaction of transactions) {
+    processTransaction(transaction);
   }
 }
 
+// Função para processar uma única transação
+function processTransaction(transaction) {
+  if (isValidTransaction(transaction)) {
+    if (transaction.type === 'PAYMENT') {
+      // Processa pagamento
+      processPayment(transaction);
+    } else if (transaction.type === 'REFUND') {
+      // Processa reembolso
+      processRefund(transaction);
+    } else {
+      console.log('Invalid transaction type!', transaction);
+    }
+  } else {
+    console.log('Invalid transaction type!', transaction);
+  }
+}
+
+// Função para verificar se a transação é válida
+function isValidTransaction(transaction) {
+  return transaction.type === 'PAYMENT' || transaction.type === 'REFUND';
+}
+
+// Função para processar transações de pagamento
+function processPayment(transaction) {
+  const paymentMethodProcessors = {
+    'CREDIT_CARD': processCreditCardPayment,
+    'PAYPAL': processPayPalPayment,
+    'PLAN': processPlanPayment,
+  };
+
+  // Verifica se há um processador válido para o método de pagamento
+  if (paymentMethodProcessors.hasOwnProperty(transaction.method)) {
+    const paymentProcessor = paymentMethodProcessors[transaction.method];
+    paymentProcessor(transaction);
+  } else {
+    console.log('Invalid payment method!', transaction);
+  }
+}
+
+// Função para processar transações de reembolso
+function processRefund(transaction) {
+  const refundMethodProcessors = {
+    'CREDIT_CARD': processCreditCardRefund,
+    'PAYPAL': processPayPalRefund,
+    'PLAN': processPlanRefund,
+  };
+
+  // Verifica se há um processador válido para o método de reembolso
+  if (refundMethodProcessors.hasOwnProperty(transaction.method)) {
+    const refundProcessor = refundMethodProcessors[transaction.method];
+    refundProcessor(transaction);
+  } else {
+    console.log('Invalid refund method!', transaction);
+  }
+}
+
+// Funções de processamento específicas para cada método de pagamento
 function processCreditCardPayment(transaction) {
-  console.log(
-    'Processing credit card payment for amount: ' + transaction.amount
-  );
+  console.log('Processing credit card payment for amount: ' + transaction.amount);
 }
 
 function processCreditCardRefund(transaction) {
-  console.log(
-    'Processing credit card refund for amount: ' + transaction.amount
-  );
+  console.log('Processing credit card refund for amount: ' + transaction.amount);
 }
 
 function processPayPalPayment(transaction) {
@@ -98,3 +130,5 @@ function processPlanPayment(transaction) {
 function processPlanRefund(transaction) {
   console.log('Processing plan refund for amount: ' + transaction.amount);
 }
+
+main();
