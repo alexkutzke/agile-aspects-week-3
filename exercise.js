653424b97ccf4797ab7543cbf35cb2dc1eb41bc5
jefersonthimoteo <jeferson.thimoteo@mega.com.br>
2023-08-26 23:09:53 -0300

    Finalizado exercicio

--- exercise.js
+++ exercise.js
@@ -1,74 +1,75 @@
+var json = require('./transactions.json');
+
+const TRANSACTION_STATUS_OPEN = "OPEN"
+
+const TRANSACTION_TYPE_PAYMENT = "PAYMENT"
+const TRANSACTION_TYPE_REFUND = "REFUND"
+
+const TRANSACTION_METHOD_CREDIT_CARD = "CREDIT_CARD"
+const TRANSACTION_METHOD_PAYPAL = "PAYPAL"
+const TRANSACTION_METHOD_PLAN = "PLAN"
 main();
 
-function main() {
-  const transactions = [
-    {
-      id: 't1',
-      type: 'PAYMENT',
-      status: 'OPEN',
-      method: 'CREDIT_CARD',
-      amount: '23.99',
-    },
-    {
-      id: 't2',
-      type: 'PAYMENT',
-      status: 'OPEN',
-      method: 'PAYPAL',
-      amount: '100.43',
-    },
-    {
-      id: 't3',
-      type: 'REFUND',
-      status: 'OPEN',
-      method: 'CREDIT_CARD',
-      amount: '10.99',
-    },
-    {
-      id: 't4',
-      type: 'PAYMENT',
-      status: 'CLOSED',
-      method: 'PLAN',
-      amount: '15.99',
-    },
-  ];
 
+function main() {
+  const transactions = json;
   processTransactions(transactions);
 }
 
 function processTransactions(transactions) {
-  if (transactions && transactions.length > 0) {
-    for (const transaction of transactions) {
-      if (transaction.type === 'PAYMENT') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardPayment(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalPayment(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanPayment(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else if (transaction.type === 'REFUND') {
-        if (transaction.status === 'OPEN') {
-          if (transaction.method === 'CREDIT_CARD') {
-            processCreditCardRefund(transaction);
-          } else if (transaction.method === 'PAYPAL') {
-            processPayPalRefund(transaction);
-          } else if (transaction.method === 'PLAN') {
-            processPlanRefund(transaction);
-          }
-        } else {
-          console.log('Invalid transaction type!', transaction);
-        }
-      } else {
-        console.log('Invalid transaction type!', transaction);
-      }
-    }
-  } else {
+  if (!transactions || transactions.length === 0) {
     console.log('No transactions provided!');
+    return false;
   }
+  for (const transaction of transactions) {
+    if (!isValid(transaction)) return;
+    if (transaction.type === TRANSACTION_TYPE_PAYMENT) {
+      processPayment(transaction);
+    } else if (transaction.type === TRANSACTION_TYPE_REFUND) {
+      processRefund(transaction)
+    }
+  }
+}
+
+function isValid(transaction) {
+  if (!isValidTransactionType(transaction)) {
+    console.log('Invalid transaction type!', transaction);
+    return false;
+  }
+  if (transaction.status !== TRANSACTION_STATUS_OPEN) {
+    console.log('Invalid transaction status!', transaction);
+    return false;
+  }
+  return true;
+}
+
+function processPayment(transaction) {
+  if (transaction.method === TRANSACTION_METHOD_CREDIT_CARD) {
+    processCreditCardPayment(transaction);
+  } else if (transaction.method === TRANSACTION_METHOD_PAYPAL) {
+    processPayPalPayment(transaction);
+  } else if (transaction.method === TRANSACTION_METHOD_PLAN) {
+    processPlanPayment(transaction);
+  }
+}
+function processRefund(transaction) {
+  if (transaction.method === TRANSACTION_METHOD_CREDIT_CARD) {
+    processCreditCardRefund(transaction);
+  } else if (transaction.method === TRANSACTION_METHOD_PAYPAL) {
+    processPayPalRefund(transaction);
+  } else if (transaction.method === TRANSACTION_METHOD_PLAN) {
+    processPlanRefund(transaction);
+  }
+}
+
+function isValidTransactionType(transaction) {
+  const validTypes = [TRANSACTION_TYPE_PAYMENT, TRANSACTION_TYPE_REFUND];
+  const validMethods = [TRANSACTION_METHOD_CREDIT_CARD, TRANSACTION_METHOD_PAYPAL, TRANSACTION_METHOD_PLAN];
+
+  return (
+    validTypes.includes(transaction.type) &&
+    validMethods.includes(transaction.method)
+  );
 }
 
 function processCreditCardPayment(transaction) {
